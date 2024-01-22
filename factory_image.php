12dfc128dcb9ad3b00e9f74319d70421b0823253
haokngoc <haooila7@gmail.com>
2024-01-22 16:01:13 +0700

    add content php

---
+++ factory_image.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="icon" href="logo.jpg" type="image">
+    <style>
+        .notification {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: #4CAF50;
+            color: #fff;
+            padding: 15px;
+            border-radius: 5px;
+            display: none;
+            z-index: 1000; /* Đảm bảo nó hiển thị trên cùng */
+        }
+    </style>
+</head>
+<body>
+
+<?php include('navigation.php'); ?>
+<div>
+    <p>
+    Under Construction
+    </p>
+</div>
+
+
+</body>
+</html>
